ee8c3751a12590af3239067a883c29fbba339553
Josh Hammer <jph33@outlook.com>
2026-09-01 16:42:20 -0400

    feat: sort global regex search results in canonical bible order (book, chapter, verse)

--- src/db.rs
+++ src/db.rs
@@ -126,6 +126,31 @@ impl Database {
             }
         }
         
+        let canonical_order = vec![
+            "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy",
+            "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel",
+            "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra",
+            "Nehemiah", "Esther", "Job", "Psalms", "Proverbs",
+            "Ecclesiastes", "Song of Solomon", "Isaiah", "Jeremiah", "Lamentations",
+            "Ezekiel", "Daniel", "Hosea", "Joel", "Amos",
+            "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk",
+            "Zephaniah", "Haggai", "Zechariah", "Malachi",
+            "Matthew", "Mark", "Luke", "John", "Acts",
+            "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians",
+            "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians",
+            "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews",
+            "James", "1 Peter", "2 Peter", "1 John", "2 John",
+            "3 John", "Jude", "Revelation"
+        ];
+
+        results.sort_by(|a, b| {
+            let a_book = canonical_order.iter().position(|&r| r == a.book).unwrap_or(100);
+            let b_book = canonical_order.iter().position(|&r| r == b.book).unwrap_or(100);
+            a_book.cmp(&b_book)
+                .then(a.chapter.cmp(&b.chapter))
+                .then(a.verse.cmp(&b.verse))
+        });
+        
         Ok(results)
     }
 }
